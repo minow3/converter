a0cb242a83058fdcff50560b62f67afc87b00cd8
minow3 <minow3@gmail.com>
2024-02-21 17:22:26 +0000

    uploaeded file

--- tools/obj/Debug/net8.0/tools.AssemblyInfo.cs
+++ tools/obj/Debug/net8.0/tools.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tools")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e2c13cb12d821742044d22dcbdb5623e98960f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("tools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
